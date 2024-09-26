89d06abd75be9040d0829f2ee8064639903bdd4b
domalasherpa <domalasherpa9813@gmail.com>
2024-09-26 16:35:33 +0545

    feat: change setup lang to ts

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="bg-gray-900 w-full fixed border-b flex justify-center h-16">
+    <div class="self-center p-4 ">
+        <ul class="flex p-4 md:p-0 mt-4 font-semibold rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+            <li>
+                <NuxtLink to="/products" 
+                class="block py-2 px-3 text-white hover:text-orange-400" aria-current="page" 
+                exact-active-class="text-orange-500"
+                >Home</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/cart" 
+                class="block py-2 px-3 text-white hover:text-orange-400" 
+                exact-active-class="text-orange-500"
+                >Carts</NuxtLink>
+            </li>
+        </ul>
+    </div>
+    </nav>
+</template>
---
+++ components/ProductSingle.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface Product{
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+}
+
+defineProps({
+    product: Object as PropType<Product>
+});
+</script>
+<template>    
+    <div v-if="product !== undefined" class="bg-white cursor-pointer flex-col h-full justify-between hover:shadow-md">
+        <div class="h-40 border-2 border-orange-200 shadow">
+            <NuxtLink :to="'/products/' + product.id">
+                <img :src="product.image" alt="product" class="h-full w-full object-contain" />
+            </NuxtLink>
+        </div>
+        <div class="px-2">
+            <NuxtLink :to="'/products/' + product.id">
+                <h5 class="text-xs font-semibold tracking-tight text-gray-900">{{ product.title }}</h5>
+            </NuxtLink>
+            <NuxtLink :to="'/products/' + product.id">
+                <p class="text-xs font-semibold text-gray-500">{{ product.price }}</p>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+</style>
