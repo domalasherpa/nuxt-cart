11f971ad908f1e436879577748b9695428f5a456
domalasherpa <domalasherpa9813@gmail.com>
2024-09-27 16:49:48 +0545

    feat: add global interface

---
+++ types/cartItem.ts
@@ -0,0 +1,12 @@
+interface Item {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    availableQuantity: number;
+    quantity:number;
+}
+
+export type {Item}
---
+++ types/product.ts
@@ -0,0 +1,11 @@
+interface Product {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+	availableQuantity: number;
+}
+
+export type { Product }
